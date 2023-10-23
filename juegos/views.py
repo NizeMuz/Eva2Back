3b671a0f5139e94e7d52120a0c284535ebbc05e1
Laboratorios Chillan <chilab@inacap.cl>
2023-10-23 15:31:53 -0300

    juegos_listos_ahora_si

--- juegos/views.py
+++ juegos/views.py
@@ -22,8 +22,8 @@ def Doom(request):
           "imagenBandera" : "doom.jpg",
           "nombre": "Nombre: Doom",
           "idioma": "Fecha de estreno: 20 de marzo de 2020",
-          "lema": "Comer o matar",
-          "continente": "Americano",
+          "lema": "Desarollador: id Software,Panic Button",
+          "continente": "Plataformas: Nintendo Switch,PlayStation 5,Xbox series X|S",
           "disabled2" : "not-active"
           }
      return render(request,'templatesApp/juegos.html', data)
@@ -32,10 +32,10 @@ def Doom(request):
 def Halo(request):
      data = {
           "imagenBandera" : "Halo.jpg",
-          "nombre": "Cuba",
-          "idioma": "Español",
-          "lema": "Patria y vida",
-          "continente": "Americano",
+          "nombre": "Nombre: Halo",
+          "idioma": "Fecha de estreno: 15 de noviembre de 2021",
+          "lema": "Desarollador: 343 Industries",
+          "continente": "Plataformas: Xbox series X|S",
           "disabled3" : "not-active"
           }
      return render(request,'templatesApp/juegos.html', data)
